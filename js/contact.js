1917c5c47c5a207014b81f8cd41cfc4269c42c2c
axavierj <alexanderjimenez29@gmail.com>
2023-05-03 13:49:26 -0400

    wip_0008

--- js/contact.js
+++ js/contact.js
@@ -1,43 +1,12 @@
+import { closeAlert } from './services/contact.service.js'
+import { handleContactFormSubmit } from './services/contact.service.js'
+
 const contactForm = document.querySelector('#contactForm')
-const contactAlert = document.querySelector('#contactAlert')
 const closeContactAlert = document.querySelector('#closeContactAlert')
 
+contactForm.addEventListener('submit', handleContactFormSubmit)
+
 closeContactAlert.addEventListener('click', () => {
-  contactAlert.close()
-})
-
-contactForm.addEventListener('submit', async (e) => {
-  e.preventDefault()
-  console.log(e.target.name.value)
-  console.log(e.target.email.value)
-  console.log(e.target.message.value)
-
-  const contact = {
-    name: e.target.name.value,
-    email: e.target.email.value,
-    message: e.target.message.value,
-  }
-
-  const contactRes = await fetch('http://localhost:5000/api/contact', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(contact),
-  })
-
-  const contactData = await contactRes.json()
-  contactAlert.showModal()
-
-  const messageRes = await fetch('http://localhost:5000/api/contact/response', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(contact),
-  })
-
-  const messageData = await messageRes.json()
-
-  e.target.reset()
+  const contactAlert = document.querySelector('#contactAlert')
+  closeAlert(contactAlert)
 })
